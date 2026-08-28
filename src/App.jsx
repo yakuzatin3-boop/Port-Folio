06f83355e91dbf3f029c4edf2b6135cc42ec1c75
Vuthin Tim <yakuzatin3@gmail.com>
2026-08-28 21:26:13 +0700

    Update Analytice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
